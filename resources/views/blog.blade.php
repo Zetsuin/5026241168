042c04783b0ed853d02f5ca7ac0e8e7ffbb51cc8
Zetsuin <rafifxy@gmail.com>
2026-04-20 12:12:00 +0700

    pertemuan7

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+
+</head>
+<body>
+    <div class="container">
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+</div>
+</body>
+</html>
